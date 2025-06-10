5ed95a159ba2b4f72a39f30152234bacd60fdb18
dgiacomo <dgiacomo@mux.com>
2025-06-10 16:52:30 +0000

    added a ton of appearance and navigation plugins

---
+++ lua/plugins/appearance.lua
@@ -0,0 +1,50 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+  },
+  {
+    -- used by go.nvim for floating windows
+    "ray-x/guihua.lua",
+  },
+  {
+    -- shows diagnosics / errors in a pettier way
+    "folke/trouble.nvim",
+  },
+  {
+    "echasnovski/mini.icons",
+  },
+  {
+    -- move a selection any direction
+    "echasnovski/mini.move",
+  },
+  {
+    "echasnovski/mini.pairs",
+  },
+  {
+    "echasnovski/mini.surround",
+  },
+  {
+    -- status column - clickable fold, etc
+    "luukvbaal/statuscol.nvim",
+  },
+  {
+    -- highlight TODOs
+    "folke/todo-comments.nvim",
+  },
+  {
+    -- compatibility layer for nvim-cmp
+    "Saghen/blink.compat",
+  },
+  {
+    -- improve quickfix window
+    "kevinhwang91/nvim-bqf",
+  },
+  {
+    -- tinier inline diagnostics
+    "rachartier/tiny-inline-diagnostic.nvim",
+  },
+  {
+    -- improved search navigation
+    "folke/flash.nvim",
+  },
+}
