0ea0dabdd25072dc41bea0bf7c730b60d674bb9e
dgiacomo <dgiacomo@mux.com>
2025-06-10 16:56:41 +0000

    adding filesystem plugins : treesitter, oil, and grapple

---
+++ lua/plugins/filesystem.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    -- edit you filesystem like its an editor window
+    "stevearc/oil.nvim",
+  },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+  },
+  {
+    -- file navigator / tagger
+    "cbochs/grapple.nvim",
+  },
+}
